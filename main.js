b6861360dd228f8f6f7533344b2e92f7ef18588d
Brendan Davis <brendanmd@gmail.com>
2017-07-19 17:07:03 -0700

    minor tweaks

--- main.js
+++ main.js
@@ -2,11 +2,21 @@
 
 var projects = [];
 //Builds the Projects - No append
-function Project(name, language, site, github, firstPublish) {
+function Project(name, language, site, github) {
   this.name = name;
   this.language = language;
-  this.firstPublish = firstPublish;
   this.site = site;
   this.github = github;
   projects.push(this);
+}
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  $newProject.removeClass('template');
+  $newProject.find('h1').html(this.name)
+  $newProject.find('#language').html(this.language)
+  $
+
+}
+
 }
---
+++ projects.js
@@ -0,0 +1,3 @@
+new Project('Codographic Memory', 'HTML/CSS/Javascript', 'www.placeholder.com', 'https://github.com/Tsarcastic/Codographic-Memory/branches');
+new Project('This', 'HTML/CSS/Javascript', 'www.placeholder.com', 'https://github.com/Tsarcastic/portfolio');
+new Project('Fake', 'HTML/CSS/Javascript', 'www.placeholder.com', 'https://github.com/Tsarcastic/notARealLink');
